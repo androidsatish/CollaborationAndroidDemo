d83e0e2df4ea89f3bea587bfe29fe29019551332
prashant2917 <jondhaleprashant@gmail.com>
2016-11-18 16:23:39 +0530

    toast messag

--- app/src/main/java/com/collaborationandroiddemo/MainActivity.java
+++ app/src/main/java/com/collaborationandroiddemo/MainActivity.java
@@ -2,11 +2,13 @@ package com.collaborationandroiddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mButton;
+    Button mButton,mBtnToast;
 
 
     @Override
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     mButton = (Button) findViewById(R.id.button);
+    mBtnToast=(Button)findViewById(R.id.buttonToast);
+        mBtnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Branch added",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
 
